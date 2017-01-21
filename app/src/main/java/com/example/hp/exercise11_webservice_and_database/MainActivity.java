ff7782bebe3e494ff203ca6304df9f8510ead61b
hp-PC\hp <Mahdi.heravi@gmail.com>
2017-01-21 23:40:35 +0330

    Add List View And Adapter to MainActivity

--- app/src/main/java/com/example/hp/exercise11_webservice_and_database/MainActivity.java
+++ app/src/main/java/com/example/hp/exercise11_webservice_and_database/MainActivity.java
@@ -7,11 +7,18 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.greenrobot.eventbus.Subscribe;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,8 +65,68 @@ public class MainActivity extends AppCompatActivity {
                 stopService(new Intent(MainActivity.this, UpdateService.class));
             }
         });
+        Button ViewDB = (Button) findViewById(R.id.viewDB);
+        ViewDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListView List1 = (ListView) findViewById(R.id.list1);
+                DBController dbController = new DBController(MainActivity.this);
+                ArrayList<news> mynews = dbController.Select_from_DB();
+                myadapter ad = new myadapter(mynews);
+                List1.setAdapter(ad);
+            }
+        });
 
 
+
+    }
+    public class myadapter extends BaseAdapter
+    {
+        ArrayList<news> mynews;
+        public myadapter(ArrayList inputnews)
+        {
+            mynews = inputnews;
+        }
+
+        @Override
+        public int getCount() {
+            return mynews.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mynews.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = getLayoutInflater();
+            View mylist = inflater.inflate(R.layout.list,parent,false);
+            TextView txtservice = (TextView) mylist.findViewById(R.id.service);
+            TextView txttitr = (TextView) mylist.findViewById(R.id.titr);
+            TextView txtjdate = (TextView) mylist.findViewById(R.id.jdate);
+            TextView txtlead = (TextView) mylist.findViewById(R.id.leade);
+            news tmp_news = new news();
+            tmp_news.setJdate(mynews.get(position).getJdate());
+            tmp_news.setTitr(mynews.get(position).getTitr());
+            tmp_news.setLead(mynews.get(position).getLead());
+            tmp_news.setService(mynews.get(position).getService());
+
+            txtjdate.setText(tmp_news.getJdate());
+            txtlead.setText(tmp_news.getLead());
+            txtservice.setText(tmp_news.getService());
+            txtjdate.setText(tmp_news.getJdate());
+
+            return mylist;
+
+
+
+        }
     }
 
     public class DBHelper2 extends SQLiteOpenHelper {
